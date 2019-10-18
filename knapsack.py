f7344a0bd5d92080facf3e746edf7b471f9750c5
allen <theallenma@gmail.com>
2019-10-17 17:08:58 -0700

    add dp solution and comparison to knapsack

--- knapsack.py
+++ knapsack.py
@@ -7,6 +7,26 @@ def knapsack_with_memo(weights, profits, capacity):
     memo = [[-1 for x in range(capacity + 1)] for y in range(len(profits))]
     return dfs_with_memo(memo, weights, profits, capacity, 0)
 
+def knapsack_dp(weights, profits, capacity):
+    dp = [[0 for x in range(capacity + 1)] for y in range(len(profits))]
+
+    # comment out because they are already 0
+    # for i in range(len(profits)):
+    #     dp[i][0] = 0
+    
+    for j in range(1, capacity + 1):
+        if weights[0] <= j:
+            dp[0][j] = profits[0]
+    
+    for i in range(1, len(profits)):
+        for j in range(1, capacity + 1):
+            if weights[i] <= j:
+                dp[i][j] = max(dp[i - 1][j], profits[i] + dp[i - 1][j - weights[i]])
+            else:
+                dp[i][j] = dp[i - 1][j]
+    
+    return dp[len(profits) - 1][capacity]
+
 def dfs(weights, profits, capacity, index):
     # base case
     if capacity < 0 or index >= len(profits):
@@ -49,3 +69,9 @@ if __name__ == "__main__":
     print(knapsack_with_memo([1, 2, 3, 5], [1, 6, 10, 16], 6))
     end = time.perf_counter()
     print(f'time for recursive function with memo: {end - start}')
+
+    start = time.perf_counter()
+    print(knapsack_dp([1, 2, 3, 5], [1, 6, 10, 16], 7))
+    print(knapsack_dp([1, 2, 3, 5], [1, 6, 10, 16], 6))
+    end = time.perf_counter()
+    print(f'time for dp: {end - start}')
