9f5fcbbefa1dc21715c9ec56b102d528e6b1497b
allen <theallenma@gmail.com>
2019-10-24 15:52:23 -0700

    dfs with memo

--- subset_sum.py
+++ subset_sum.py
@@ -1,5 +1,7 @@
 def func1(arr, target):
-    return dfs(arr, 0, target)
+    print(f'dfs: {dfs(arr, 0, target)}')
+    dp = [[-1 for x in range(target + 1)] for y in range(len(arr))]
+    print(f'dfs2: {True if dfs2(arr, dp, 0, target) == 1 else False}')    
 
 def dfs(arr, index, remain):
     if len(arr) == 0 or index >= len(arr) or remain < 0:
@@ -9,8 +11,25 @@ def dfs(arr, index, remain):
 
     return dfs(arr, index + 1, remain - arr[index]) or dfs(arr, index + 1, remain)
 
+def dfs2(arr, dp, index, remain):
+    if len(arr) == 0 or index >= len(arr) or remain < 0:
+        return 0
+    if remain == 0:
+        return 1
+    if dp[index][remain] != -1:
+        return dp[index][remain]
+    
+    # include myself
+    if dfs2(arr, dp, index + 1, remain - arr[index]) == 1:
+        dp[index][remain] = 1
+        return 1
+    
+    # not include myself
+    dp[index][remain] = 1 if dfs2(arr, dp, index + 1, remain) == 1 else 0
+    return dp[index][remain]
+
 if __name__ == '__main__':
-    print(f'func1: {func1([1, 2, 3, 7], 6)}')
-    print(f'func1: {func1([1, 2, 7, 1, 5], 10)}')
-    print(f'func1: {func1([3, 1, 4, 8], 6)}')
+    func1([1, 2, 3, 7], 6)
+    func1([1, 2, 7, 1, 5], 10)
+    func1([3, 1, 4, 8], 6)
     
