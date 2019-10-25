196c1c14ce90f7cfe890511e1f09fe61b55bc1bc
allen <theallenma@gmail.com>
2019-10-25 00:57:04 -0700

    add dp solution (with a bug fixed)

--- subset_sum.py
+++ subset_sum.py
@@ -1,7 +1,24 @@
 def func1(arr, target):
     print(f'dfs: {dfs(arr, 0, target)}')
+
     dp = [[-1 for x in range(target + 1)] for y in range(len(arr))]
     print(f'dfs2: {True if dfs2(arr, dp, 0, target) == 1 else False}')    
+
+    dp = [[False for x in range(target + 1)] for y in range(len(arr))]
+    for i in range(len(arr)):
+        dp[i][0] = True
+    for j in range(1, target + 1):
+        ## This place I had a bug caused by dp[0][j] = arr[0] == target
+        dp[0][j] = arr[0] == j
+
+    for i in range(1, len(arr)):
+        for j in range(1, target + 1):
+            if dp[i - 1][j]:
+                dp[i][j] = True
+            elif j >= arr[i]:
+                dp[i][j] = dp[i - 1][j - arr[i]]
+    
+    print(f'dp: {dp[len(arr) - 1][target]}')
 
 def dfs(arr, index, remain):
     if len(arr) == 0 or index >= len(arr) or remain < 0:
@@ -32,4 +49,4 @@ if __name__ == '__main__':
     func1([1, 2, 3, 7], 6)
     func1([1, 2, 7, 1, 5], 10)
     func1([3, 1, 4, 8], 6)
-    
+        
