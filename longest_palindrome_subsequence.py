41e27ca66d89b4c817b498a9270c2a3dc925a577
allen <theallenma@gmail.com>
2019-10-31 15:55:47 -0700

    add dp soln

--- longest_palindrome_subsequence.py
+++ longest_palindrome_subsequence.py
@@ -20,6 +20,20 @@ def func2(s):
     memo = [[-1 for _ in range(len(s))] for _ in range(len(s))]
     return dfs2(s, memo, 0, len(s) - 1)
 
+def func3(s):
+    n = len(s)
+    dp = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        dp[i][i] = 1
+    # notice the sequence of execution
+    for start in range(n - 1, -1, -1):
+        for end in range(start + 1, n):
+            if s[start] == s[end]:
+                dp[start][end] = 2 + dp[start + 1][end - 1]
+            else:
+                dp[start][end] = max(dp[start + 1][end], dp[start][end - 1])
+    return dp[0][n - 1]
+
 def dfs(s, start, end):
     if start > end:
         return 0
@@ -54,12 +68,16 @@ def dfs2(s, memo, start, end):
 if __name__ == '__main__':
     test(func1, 1)
     test(func2, 1)
+    test(func3, 1)
 
     test(func1, 2)
     test(func2, 2)
+    test(func3, 2)
 
     test(func1, 3)
     test(func1, 3)
+    test(func3, 3)
 
     test(func1, 4)
     test(func2, 4)
+    test(func3, 4)
